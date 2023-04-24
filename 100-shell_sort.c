3dd59a7c5285e11259a04d89dd278345c4f25c05
Ivy <ivystacyn@gmail.com>
2023-04-24 08:19:17 -0700

    Task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -23,32 +23,17 @@ void swap(int *x, int *y)
  */
 void shell_sort(int *array, size_t size)
 {
-	int i, j, temp;
-	size_t interval = 1;
-
 	if (array == NULL || size < 2)
 		return;
 
-	while (interval <= size / 3)
+	for (size_t interval = size / 2; interval > 0; interval /= 2)
 	{
-		interval = interval * 3 + 1;
-	}
-
-	while (interval > 0)
-	{
-		for (i = interval; i < size; i++)
+		for (size_t i = interval; i < size; ++i)
 		{
-			temp = array[i];
-			j = i;
-			while (j >= interval && array[j - interval] > temp)
-			{
+			for (size_t j = i; j >= interval && array[j - interval] > array[j];
+					j -= interval)
 				swap(&array[j], &array[j - interval]);
-				j -= interval;
-			}
-			array[j] = temp;
 		}
 		print_array(array, size);
-
-		interval = (interval - 1) / 3;
 	}
 }
