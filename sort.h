c991d7d89ffc7d2c7bb41f713af19bc2d7fb6122
Ivy <ivystacyn@gmail.com>
2023-04-25 05:49:44 -0700

    Task 4

--- sort.h
+++ sort.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Comparison direction macros for bitonic sort*/
+#define UP 0
+#define DOWN 1
 
 /**
  * enum bool - Enumeration of Boolean values.
@@ -37,5 +40,13 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /*SORT_H*/
